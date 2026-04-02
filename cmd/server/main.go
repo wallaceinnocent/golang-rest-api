e744b5fd28e3c1852a8ecb7ac35b69e6c30e0ecd
wallaceinnocent <eng.wallaceinnocent@gmail.com>
2026-04-02 23:24:03 +0300

    adds get req

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v3"
 )
 
 func init() {
-	log.Fatal("Startes")
+	log.Println("Starting ....")
 }
 func main() {
 	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.Status(http.StatusOK).JSON(fiber.Map{
+			"message": "Hello world",
+		})
+	})
 	app.Listen(":8080")
 
 }
