5a7c7973df5115bc0023c3d9da8830ee57d93f1b
wallaceinnocent <eng.wallaceinnocent@gmail.com>
2026-04-02 22:53:16 +0300

    spins up fiber server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,30 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"main/internals/models"
-)
+import "github.com/gofiber/fiber/v3"
 
 func main() {
-	name := another()
-	fmt.Println(name)
+	app := fiber.New()
+	app.Listen(":8080")
 
-}
-
-func another() string {
-
-	vehicle := models.Vehicle{RegNo: 1, Name: "crown", Model: "toyota"}
-	user := &models.Users{}
-
-	user.Age = 10
-	user.Name = "User"
-	user.VehicleReg = vehicle.RegNo
-
-	changeStruct(user)
-
-	return user.Name
-}
-
-func changeStruct(user *models.Users) {
-	user.Name = "User2"
 }
